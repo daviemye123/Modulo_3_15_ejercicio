b27bad392505bdf29a415d1b214682110eff2d5b
david pedraza <davlpedraza@soy.sena.edu.co>
2025-11-06 10:53:41 -0500

    Correccion calcular impuesto

--- Calculadora_impuesto.py
+++ Calculadora_impuesto.py
@@ -1,11 +1,14 @@
 TASA_IVA = 0.19
 
+
 def calcular_iva(precio_base: float) -> float:
     return precio_base * TASA_IVA
+
 
 def actualizar_iva(nueva_tasa: float):
     global TASA_IVA
     TASA_IVA = nueva_tasa
+
 
 if __name__ == "__main__":
     precio_base = 100.00
@@ -13,9 +16,9 @@ if __name__ == "__main__":
     print(f"Precio Base: ${precio_base:.2f}")
 
     iva_inicial = calcular_iva(precio_base)
-    print(f"Cálculo inicial (Tasa {TASA_IVA*100:.0f}%): ${iva_inicial:.2f}")
+    print(f"Cálculo inicial (Tasa {TASA_IVA * 100:.0f}%): ${iva_inicial:.2f}")
 
     actualizar_iva(0.21)
     print(f"Tasa de IVA global después de actualizar: {TASA_IVA}")
     iva_actualizado = calcular_iva(precio_base)
-    print(f"Cálculo actualizado (Tasa {TASA_IVA*100:.0f}%): ${iva_actualizado:.2f}")
+    print(f"Cálculo actualizado (Tasa {TASA_IVA * 100:.0f}%): ${iva_actualizado:.2f}")
