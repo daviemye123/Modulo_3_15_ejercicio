f33660ae64acc6d1de3b70d2bdd06f25e85a3180
david pedraza <davlpedraza@soy.sena.edu.co>
2025-11-06 11:10:36 -0500

    correccion filtro

--- Filtrado_estudiantes_filter_7.py
+++ Filtrado_estudiantes_filter_7.py
@@ -1,8 +1,9 @@
 def main():
     estudiantes = [("Ana", 4.5), ("Juan", 2.8), ("Maria", 3.9)]
-    nuw_list=filter(lambda x: x [1]>= 3.0, estudiantes)
+    nuw_list = filter(lambda x: x[1] >= 3.0, estudiantes)
     lista_aprobados = list(nuw_list)
     print(f"Estudiantes: {lista_aprobados}")
+
 
 if __name__ == "__main__":
     main()
