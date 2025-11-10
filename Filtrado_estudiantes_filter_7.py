6a9cfb17e7de7d763ba76cb15975c6d593fb8660
david pedraza <davlpedraza@soy.sena.edu.co>
2025-11-10 13:14:12 -0500

    correccion ruff de filtrado_estudiantes

--- Filtrado_estudiantes_filter_7.py
+++ Filtrado_estudiantes_filter_7.py
@@ -1,6 +1,7 @@
 def main():
     estudiantes = [("Ana", 4.5), ("Juan", 2.8), ("Maria", 3.9)]
-    nuw_list = filter(lambda x: x[1] >= 3.0, estudiantes)
+    nota=3.0
+    nuw_list = filter(lambda x: x[1] >= nota, estudiantes)
     lista_aprobados = list(nuw_list)
     print(f"Estudiantes: {lista_aprobados}")
 
